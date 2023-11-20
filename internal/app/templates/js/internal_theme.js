55a3219530aa0d50cb954b4fe76a976269c7fe31
Ilya Dobrynin <mr.woodysimpson@gmail.com>
2023-11-20 15:23:54 +0300

    theme script updated

--- internal/app/templates/js/internal_theme.js
+++ internal/app/templates/js/internal_theme.js
@@ -5,5 +5,7 @@ if (tg != null) {
     document.documentElement.style.setProperty('--background-color', tg.themeParams.bg_color);
     document.documentElement.style.setProperty('--text-color', tg.themeParams.text_color);
     document.documentElement.style.setProperty('--text-hint-color', tg.themeParams.hint_color);
+    document.documentElement.style.setProperty('--button-color', tg.themeParams.button_color);
+    document.documentElement.style.setProperty('--button-text-color', tg.themeParams.button_text_colorString);
 
 }
